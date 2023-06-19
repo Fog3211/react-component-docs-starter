21fb4f8ab747003f1fd724d09300c40b938db617
renqingzhong <23151576+Fog3211@users.noreply.github.com>
2023-06-19 18:05:01 +0800

    chore: inti project

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-styling",
+    "@storybook/addon-actions",
+    "@storybook/addon-interactions"
+  ],
+  docs: {
+    autodocs: true
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+};
+
+export default config;
